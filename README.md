01cc5c8e266c9e0645d07cb7e54a126f1a56f663
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-08-05 13:22:13 -0400

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+# this is the deployable app made as a template
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("web 44 rocks");
+require("dotenv").config();
+const { PORT } = require("./config");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.use("*", (req, res) => {
+	res.json({
+		message: "web 44 is awesome indeed",
+	});
+});
+
+server.listen(PORT, () => {
+	console.log(`listening on ${PORT}`);
+});
